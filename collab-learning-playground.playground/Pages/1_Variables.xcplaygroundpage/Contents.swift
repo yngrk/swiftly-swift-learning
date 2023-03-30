3eda579614d589402bf031a2e1e4d5b82ae65cba
Youngrok Kim <yngrk.kim@gmail.com>
2023-03-30 20:24:26 +0200

    folder structure, added Variables page

---
+++ collab-learning-playground.playground/Pages/1_Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+
+// variable definitions
+var greeting = "Hello World!" // creates new mutable variable
+greeting = "this works"
+
+let constant = "This is a constant" // creates a constant
+// constant = "this does not work"
+
+
+/*
+ semicolons are optional and only required when
+ e.g defining multiple variables on one line
+*/
